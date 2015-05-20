2d56390b7ec7d5b3f7c83713ea9d6ae5db06170f
Saul St. John <saul@thundergate.io>
2015-05-20 13:27:51 -0500

    some debug magics

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
    #    #    # #    # #   ## #    # #      #   #  #     # #    #   #   #
    #    #    #  ####  #    # #####  ###### #    #  #####  #    #   #   ######
 
-                                 Version 0.4.1
+                                 Version 0.4.2
                         Copyright (c) 2015 Saul St John
 
                             <a href="http://thundergate.io">http://thundergate.io</a>
--- py/cpu.py
+++ py/cpu.py
@@ -64,69 +64,92 @@ def to_x(v):
     
 
 class Cpu(rflip.cpu):
+    def tr_read(self, addr, count):
+        if addr >= 0x08000000 and addr < 0x08010000:
+            raddr = (addr & 0xffff) | 0x30000
+        elif addr >= 0x40000000 and addr < 0x40005000:
+            raddr = (addr & 0xffff) | 0x20000
+        elif addr >= 0xc0000000:
+            raddr = addr & 0xffff
+        
+        try:
+            tmp = []
+            for i in range(0, count * 4, 4):
+                self._dev.pci.reg_base_addr = raddr + i
+                _ = self._dev.pci.reg_base_addr
+                tmp += [htonl(self._dev.pci.reg_data)]
+
+            tmp = (c_uint * count)(*tmp)
+            return cast(pointer(tmp), POINTER(c_char * (count * 4))).contents.raw
+        except:
+            return self._dev.mem.read(addr, count * 4)
+
     if not _no_capstone:
         md_mode = CS_MODE_MIPS32 + CS_MODE_BIG_ENDIAN
         md = Cs(CS_ARCH_MIPS, md_mode)
         md.detail = True
         md.skipdata = True
 
-        def dis_at(self, addr, count=1, verbose=0, pc=None):
+        def dis_at(self, addr, count=1, verbose=0, pc=None, bp=None):
             if pc is None:
                 pc = self.pc
-
-            if addr >= 0x08000000 and addr < 0x08010000:
-                raddr = (addr & 0xffff) | 0x30000
-            elif addr >= 0x40000000 and addr < 0x40005000:
-                raddr = (addr & 0xffff) | 0x20000
             
-            try:
-                tmp = []
-                for i in range(0, count * 4, 4):
-                    self._dev.pci.reg_base_addr = raddr + i
-                    _ = self._dev.pci.reg_base_addr
-                    tmp += [htonl(self._dev.pci.reg_data)]
-
-                tmp = (c_uint * count)(*tmp)
-                raw = cast(pointer(tmp), POINTER(c_char * (count * 4))).contents.raw
-            except:
-                raw = self._dev.mem.read(addr, count * 4)
+            raw = self.tr_read(addr, count)
 
             dis = self.md.disasm(raw, addr)
-            dis = self._disp_dis(dis, verbose=verbose, pc=pc)
+            dis = self._disp_dis(dis, verbose=verbose, pc=pc, bp=bp)
 
 
         def dis_pc(self, context=0, verbose=1):
             if not self.status.halted:
                 raise Exception("cpu not halted")
+            pc = self.pc
+            bp = self.breakpoint.address
             if context != 0:
-                pc = self.pc
-                self.dis_at(pc - context, count = context * 2, pc = pc)
+                self.dis_at(pc - (context * 4), count = (context * 2) + 1, pc = pc, bp = bp)
             else:
-                self.dis_at(self.pc, verbose=verbose, pc=self.pc)
+                self.dis_at(self.pc, verbose=verbose, pc = pc, bp = bp)
 
-        def _disp_dis(self, decoded, verbose=0, pc=None):
+        def _disp_dis(self, decoded, verbose=0, pc=None, bp=None):
             for i in decoded:
-                tmp = '\t'
+                tmp = '    '
                 if i.address == pc:
-                    tmp = '-->\t'
-                print "%s0x%08x:\t%s\t%s" % (tmp, i.address, i.mnemonic, i.op_str)
+                    tmp = '--> '
+                if i.address == bp:
+                    tmp = '*' + tmp[1:]
+                print "%s0x%08x:  %08x  %s  %s" % (tmp, i.address, struct.unpack(">I", i.bytes)[0], i.mnemonic, i.op_str)
                 if verbose and len(i.operands) > 0:
                      c = -1
                      for j in i.operands:
                          c += 1
                          if j.type == MIPS_OP_REG:
                              val = getattr(self, "r%d" % (j.reg - 1))
-                             print("\t\toperand %u: REG %s (= %08x)" % (c, i.reg_name(j.reg), val))
+                             print(" " * 19 + "operand %u: REG %s = %08x" % (c, i.reg_name(j.reg), val))
                          if j.type == MIPS_OP_IMM:
-                             print("\t\toperand %u: IMM = %s" % (c, to_x(j.imm)))
+                             print(" " * 19 + "operand %u: IMM = %s" % (c, to_x(j.imm)))
                          if j.type == MIPS_OP_MEM:
-                             print("\t\toperand %u: MEM" % c)
+                             addr = 0
+                             val = 0
+                             disp = 0
+                             eff = 0
                              if j.mem.base != 0:
-                                 print("\t\t\toperand %u mem.base: REG %s"
-                                     % (c, i.reg_name(j.mem.base)))
+                                 val = getattr(self, "r%d" % (j.reg - 1))
+                                 addr = val
                              if j.mem.disp != 0:
-                                 print("\t\t\toperands[%u].mem.disp: %s"
-                                     % (c, to_x(j.mem.disp)))
+                                 disp = int(j.mem.disp)
+                                 addr += disp
+                             if addr != 0:
+                                 eff = struct.unpack(">I", self.tr_read(addr, 1))[0]
+                             print(" " * 19 + "operand %u: MEM @%08x = %08x" % (c, addr, eff))
+                             if j.mem.base != 0:
+                                 val = getattr(self, "r%d" % (j.reg - 1))
+                                 print(" " * 21 + "operand %u base: REG %s = %08x"
+                                     % (c, i.reg_name(j.mem.base), val))
+                                 addr = val
+                             if j.mem.disp != 0:
+                                 print(" " * 21 + "operand %u disp: IMM %x"
+                                     % (c, int(j.mem.disp)))
+                                 addr += j.mem.disp
 
     def set_breakpoint(self, addr, enable = True, reset = False):
         if not self.breakpoint.disabled and not reset:
@@ -251,6 +274,28 @@ class Cpu(rflip.cpu):
 
                 @line_magic
                 def ir(self, arg): return self.cpu.instruction
+
+                @line_magic
+                def rh(self, arg):
+                    self.cpu._dev.grc.fastboot_pc.enable = 0
+                    self.cpu.status.word = 0xffffffff
+                    self.cpu.mode.reset = 1
+                    self.cpu.mode.halt = 1
+                    self.u(None)
+
+                @line_magic
+                def su(self, arg):
+                    if self.cpu.status.halted != 1:
+                        raise Exception("halt cpu first")
+                    lpc = self.cpu.pc
+                    t = eval('lambda x: x.%s' % arg)
+                    
+                    while not t(self.cpu):
+                        lpc = self.cpu.pc
+                        self.cpu.mode.single_step = 1
+
+                    print "%s became true at %08x" % (str(arg), lpc)
+                    self.u(lpc)
 
         print "[+] loading %s debug magics" % self.block_name
         ip = get_ipython()
