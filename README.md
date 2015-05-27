df5b2409ca13a8dcee0b076e126382d4875c2d09
Saul St. John <saul@thundergate.io>
2015-05-27 09:40:30 -0500

    cpmu fixes

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
    #    #    # #    # #   ## #    # #      #   #  #     # #    #   #   #
    #    #    #  ####  #    # #####  ###### #    #  #####  #    #   #   ######
 
-                                 Version 0.6.1
+                                 Version 0.6.2
                         Copyright (c) 2015 Saul St John
 
                             <a href="http://thundergate.io">http://thundergate.io</a>
--- include/cpmu.h
+++ include/cpmu.h
@@ -20,18 +20,19 @@
 #define _CPMU_H_
 
 struct cpmu_control {
-    u32 reserved1 :1;
-    u32 reserved2 :1;
-    u32 reserved3 :1;
+    u32 reserved31 :1;
+    u32 reserved30 :1;
+    u32 reserved29 :1;
     u32 always_force_gphy_dll_on :1;
     u32 enable_gphy_powerdown_in_dou :1;
-    u32 reserved4 :1;
-    u32 reserved5 :1;
-    u32 reserved6 :1;
-    u32 reserved7 :1;
-    u32 reserved8 :1;
+    u32 reserved26 :1;
+    u32 reserved25 :1;
+    u32 reserved24 :1;
+    u32 reserved23 :1;
+    u32 sw_ctrl_ape_reset :1;
+    u32 sw_ctrl_core_reset :1;
     u32 media_sense_power_mode_enable :1;
-    u32 reserved9 :1;
+    u32 reserved19 :1;
     u32 legacy_timer_enable :1;
     u32 frequency_multiplier_enable :1;
     u32 gphy_10mb_receive_only_mode_enable :1;
@@ -43,16 +44,20 @@ struct cpmu_control {
     u32 card_reader_idle_enable :1;
     u32 card_read_iddq :1;
     u32 lan_iddq :1;
-    u32 reserved10 :3;
+    u32 ape_deep_sleep_mode_en :1;
+    u32 ape_sleep_mode_en :1;
+    u32 reserved3 :1;
     u32 power_down :1;
     u32 register_software_reset :1;
     u32 software_reset :1;
 };
 
 struct cpmu_clock {
-    u32 reserved :11;
+    u32 reserved21 :11;
     u32 mac_clock_switch :5;
-    u32 reserved2 :16;
+    u32 reserved13 :3;
+    u32 ape_clock_switch :5;
+    u32 reserved0 :8;
 };
 
 struct cpmu_override {
@@ -77,6 +82,21 @@ struct cpmu_status {
     u32 pm_state_machine_state :4;
 };
 
+struct cpmu_clock_status {
+	u32 reserved30 :2;
+	u32 flash_clk_dis :1;
+	u32 reserved26 :3;
+	u32 ape_hclk_dis :1;
+	u32 ape_fclk_dis :1;
+	u32 reserved21 :3;
+	u32 mac_clk_sw :5;
+	u32 reserved13 :3;
+	u32 ape_clk_sw :5;
+	u32 reserved7 :1;
+	u32 flash_clk_sw :3;
+	u32 reserved0 :4;
+};
+
 struct cpmu_pcie_status {
     u32 dl_active :1;
     u32 debug_vector_sel_2 :4;
@@ -84,6 +104,102 @@ struct cpmu_pcie_status {
     u32 phylinkup :1;
     u32 debug_vector_sel_1 :4;
     u32 debug_vector_1 :11;
+};
+
+struct cpmu_gphy_control_status {
+    u32 logan_sku :3;
+    u32 reserved14 :15;
+    u32 gphy_10mb_rcv_only_mode_tx_idle_debounce_timer :2;
+    u32 reserved11 :1;
+    u32 dll_iddq_state :1;
+    u32 pwrdn :1;
+    u32 set_bias_iddq :1;
+    u32 force_dll_on :1;
+    u32 dll_pwrdn_ok :1;
+    u32 sw_ctrl_por :1;
+    u32 reset_ctrl :1;
+    u32 reserved3 :1;
+    u32 dll_handshaking_disable :1;
+    u32 bias_iddq :1;
+    u32 phy_iddq :1;
+};
+
+struct cpmu_ram_control {
+	u32 core_ram_power_down :1;
+	u32 bd_ram_power_down :1;
+	u32 reserved22 :8;
+	u32 disable_secure_fw_loading_status :1;
+	u32 remove_lan_function :1;
+	u32 reserved18 :2;
+	u32 hide_function_7 :1;
+	u32 hide_function_6 :1;
+	u32 hide_function_5 :1;
+	u32 hide_function_4 :1;
+	u32 hide_function_3 :1;
+	u32 hide_function_2 :1;
+	u32 hide_function_1 :1;
+	u32 hide_function_0 :1;
+	u32 reserved8 :2;
+	u32 ram_bank7_dis :1;
+	u32 ram_bank6_dis :1;
+	u32 ram_bank5_dis :1;
+	u32 ram_bank4_dis :1;
+	u32 ram_bank3_dis :1;
+	u32 ram_bank2_dis :1;
+	u32 ram_bank1_dis :1;
+	u32 ram_bank0_dis :1;
+};
+
+struct cpmu_cr_idle_det_debounce_ctrl {
+	u32 reserved16 :16;
+	u32 timer :16;
+};
+
+struct cpmu_core_idle_det_debounce_ctrl {
+	u32 reserved8 :24;
+	u32 timer :8;
+};
+
+struct cpmu_pcie_idle_det_debounce_ctrl {
+	u32 reserved8 :24;
+	u32 timer :8;
+};
+
+struct cpmu_energy_det_debounce_ctrl {
+	u32 reserved10 :22;
+	u32 energy_detect_select :1;
+	u32 select_hw_energy_det :1;
+	u32 select_sw_hw_oring_energy_det :1;
+	u32 sw_force_energy_det_value :1;
+	u32 disable_energy_det_debounce_low :1;
+	u32 disable_energy_det_debounce_high :1;
+	u32 energy_det_debounce_high_limit :2;
+	u32 energy_det_debounce_low_limit :2;
+};
+
+struct cpmu_dll_lock_timer {
+	u32 reserved12 :20;
+	u32 gphy_dll_lock_dimer_enable :1;
+	u32 gphy_dll_lock_timer :11;
+};
+
+struct cpmu_chip_id {
+	u32 chip_id_hi :4;
+	u32 chip_id_lo :16;
+	u32 base_layer_revision :4;
+	u32 metal_layer_revision :8;
+};
+
+struct cpmu_mutex {
+	u32 reserved13 :19;
+	u32 req_12 :1;
+	u32 reserved9 :3;
+	u32 req_8 :1;
+	u32 reserved5 :3;
+	u32 req_4 :1;
+	u32 reserved3 :1;
+	u32 req_2 :1;
+	u32 reserved0 :2;
 };
 
 struct cpmu_padring_control {
@@ -134,22 +250,22 @@ struct cpmu_regs {
     struct cpmu_override override_enable;
     struct cpmu_status status;
     
-    struct cpmu_clock clock_status;
+    struct cpmu_clock_status clock_status;
     struct cpmu_pcie_status pcie_status;
-    u32 gphy_control_status;
-    u32 ram_control;
+    struct cpmu_gphy_control_status gphy_control_status;
+    struct cpmu_ram_control ram_control;
     
-    u32 cr_idle_detect_debounce_ctrl;
+    struct cpmu_cr_idle_det_debounce_ctrl cr_idle_detect_debounce_ctrl;
     u32 eee_debug;
-    u32 core_idle_detect_debounce_ctrl;
-    u32 pcie_idle_detect_debounce_ctrl;
+    struct cpmu_core_idle_det_debounce_ctrl core_idle_detect_debounce_ctrl;
+    struct cpmu_pcie_idle_det_debounce_ctrl pcie_idle_detect_debounce_ctrl;
     
-    u32 energy_detect_debounce_timer;
-    u32 dll_lock_timer;
-    u32 chip_id;
-    u32 mutex_request;
+    struct cpmu_energy_det_debounce_ctrl energy_detect_debounce_timer;
+    struct cpmu_dll_lock_timer dll_lock_timer;
+    struct cpmu_chip_id chip_id;
+    struct cpmu_mutex mutex_request;
     
-    u32 mutex_grant;
+    struct cpmu_mutex mutex_grant;
     u32 ofs_64;
     struct cpmu_padring_control padring_control;
     u32 ofs_6c;
