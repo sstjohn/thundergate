20687f8265f7af462c23802a2028498000ae5238
Saul St John <saul.stjohn@gmail.com>
2016-10-21 16:06:06 -0500

    or even

--- py/cdpserver.py
+++ py/cdpserver.py
@@ -247,15 +247,18 @@ class CDPServer(object):
 
     def __advance_to_next_line(self):
         current_pc = self.dev.rxcpu.pc
-        if not current_pc in self._bp_replaced_insn:
-            ir_reg_val = self.dev.rxcpu.ir
-            insn_from_mem = struct.unpack(">I", self.dev.rxcpu.tr_read(current_pc, 1))[0]
-            if ir_reg_val != insn_from_mem:
-                print "ir reg is %x, should be %x, fixing." % (ir_reg_val, insn_from_mem)
-                self.dev.rxcpu.ir = insn_from_mem
-                ir_reg_val = self.dev.rxcpu.ir
-            assert ir_reg_val == insn_from_mem
+
         while True:
+            if not current_pc in self._bp_replaced_insn:
+                ir_reg_val = self.dev.rxcpu.ir
+                insn_from_mem = struct.unpack(">I", self.dev.rxcpu.tr_read(current_pc, 1))[0]
+                if ir_reg_val != insn_from_mem:
+                    print "ir reg is %x, should be %x, fixing." % (ir_reg_val, insn_from_mem)
+                    self.dev.rxcpu.ir = insn_from_mem
+                    ir_reg_val = self.dev.rxcpu.ir
+                assert ir_reg_val == insn_from_mem
+            else:
+                self.__prepare_resume_from_breakpoint()
             self.dev.rxcpu.mode.single_step = 1
             count = 0
             while self.dev.rxcpu.mode.single_step:
